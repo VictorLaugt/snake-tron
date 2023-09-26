f39715a3ed3440b43f45630123843fa1291ce845
LAUGT Victor <Victor.Laugt@bordeaux-inp.fr>
2023-09-26 11:47:19 +0200

    Adjusted default values for initialization

--- __main__.py
+++ __main__.py
@@ -1,10 +1,12 @@
+import argparse
+
 def launch_user_game():
     import snake_back, snake_front
     
     # world = snake_back.SnakeWorld(
     world = snake_back.PeriodicSnakeWorld(
-        world_width=20, world_height=20,
-        food_number=4,
+        world_width=10, world_height=10,
+        food_number=2,
         initial_snake=((1, 1), (1, 2), (1, 3), (1, 4), (1, 5)),
         initial_dir=snake_back.UP
     )
@@ -39,5 +41,11 @@ def launch_ai_game():
     graphic_ui.mainloop()
 
 
-# launch_user_game()
-launch_ai_game()
+parser = argparse.ArgumentParser()
+parser.add_argument('-a', '--ai', action='store_true')
+args = parser.parse_args()
+if args.ai:
+    launch_ai_game()    
+else:
+    launch_user_game()
+
