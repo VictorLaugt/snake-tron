272159ef7c4bd816374fdba4fb420b3b9c0a4e37
LAUGT Victor <Victor.Laugt@bordeaux-inp.fr>
2024-12-22 18:57:09 +0100

    singleplayer versus twoplayers

--- __main__.py
+++ __main__.py
@@ -18,18 +18,21 @@ le plus court vers la pomme la plus proche.
 """
 
 parser = argparse.ArgumentParser()
-parser.add_argument('mode', choices=('singleplayer', 'versus'), nargs='?', default='singleplayer')
+parser.add_argument('mode', choices=('singleplayer', 'versus', 'twoplayers'), nargs='?', default='singleplayer')
 parser.add_argument('-e', '--explain-ai', action='store_true')
 parser.add_argument('-t', '--time-step', type=int, default=100)
 args = parser.parse_args()
 
-world = world.SnakeWorld(width=20, height=20, n_food=4)
+world = world.SnakeWorld(width=30, height=30, n_food=4)
 
-agent_1_initial_pos = ((1,1), (1,2), (1,3), (1,4), (1,5))
-agent_1_initial_dir = (0,-1)
+agent_1_initial_pos = ((2,5), (2,4), (2,3), (2,2), (2,1))
+agent_1_initial_dir = (0,1)
 
-agent_2_initial_pos = ((3,1), (3,2), (3,3), (3,4), (3,5))
+agent_2_initial_pos = ((5,1), (5,2), (5,3), (5,4), (5,5))
 agent_2_initial_dir = (0,-1)
+
+agent_3_initial_pos = ((8,5), (8,4), (8,3), (8,2), (8,1))
+agent_3_initial_dir = (0,1)
 
 player_agents = []
 ai_agents = []
@@ -37,8 +40,12 @@ ai_agents = []
 player_agents.append(world.new_player_agent(agent_1_initial_pos, agent_1_initial_dir))
 if args.mode == 'singleplayer':
     ai_agents.append(world.new_a_star_agent(agent_2_initial_pos, agent_2_initial_dir))
-else:
+elif args.mode == 'versus':
     player_agents.append(world.new_player_agent(agent_2_initial_pos, agent_2_initial_dir))
+elif args.mode == 'twoplayers':
+    ai_agents.append(world.new_a_star_agent(agent_2_initial_pos, agent_2_initial_dir))
+    player_agents.append(world.new_player_agent(agent_3_initial_pos, agent_3_initial_dir))
+
 
 gui = front.SnakeGameWindow(
     world,
